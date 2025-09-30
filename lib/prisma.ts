4d2492c9a29c9592de4aa1d4ec5d996e1ece72a8
Aidan McAlister <aidankmcalister@gmail.com>
2025-09-30 14:10:46 -0400

    datasource added

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "../app/generated/prisma/client.ts";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: Deno.env.get("DATABASE_URL"),
+});
 
 export default prisma;
